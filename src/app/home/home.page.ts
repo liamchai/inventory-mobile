3b2d4102658da65757131dbfee8149603c7901c1
William <williamliang99@gmail.com>
2020-09-26 11:44:16 +0700

    WIP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  userPin: number;
+  db: any;
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
 
-  constructor() {}
+  constructor(private databaseService: DatabaseService) {
+    this.db = this.databaseService.db;
+  }
 
+  onLoginButtonClick() {
+    console.log(this.userPin);
+  }
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
 }
