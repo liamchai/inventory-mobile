225320102f0ed6a999c44ca107d9bed6b144155c
William <williamliang99@gmail.com>
2020-09-27 21:47:44 +0700

    implement guard

--- src/app/inventory/main/main-routing.module.ts
+++ src/app/inventory/main/main-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from 'src/app/guards/authentication.guard';
 import { MainPage } from './main.page';
 
 const routes: Routes = [
@@ -13,6 +14,7 @@ const routes: Routes = [
           import('../dashboard/dashboard.module').then(
             (m) => m.DashboardPageModule
           ),
+        canActivate: [AuthenticationGuard],
       },
     ],
   },
