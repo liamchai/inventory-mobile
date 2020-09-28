8e55391769c27860c595e67378139aba76ea0013
William <williamliang99@gmail.com>
2020-09-28 16:49:27 +0700

    added showcase mockup

---
+++ src/app/inventory/showcase/showcase.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
+
+@Component({
+  selector: 'app-showcase',
+  templateUrl: './showcase.page.html',
+  styleUrls: ['./showcase.page.scss'],
+})
+export class ShowcasePage implements OnInit {
+  items: Item[];
+  constructor() {
+    this.items = [
+      { name: 'faster', color: 'red', quantity: 10 },
+      { name: 'faster', color: 'blue', quantity: 10 },
+      { name: 'snowman', color: 'white', quantity: 20 },
+      { name: 'pewarna', color: 'white', quantity: 20 },
+    ];
+  }
+
+  ngOnInit() {}
+}
