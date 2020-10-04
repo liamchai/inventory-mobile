8a747dd196ab25340bde89ae3d5b85d82b9eec75
William <williamliang99@gmail.com>
2020-10-04 23:48:51 +0700

    change hardcoded data into backend data and remove comments

--- src/app/inventory/report/report.page.ts
+++ src/app/inventory/report/report.page.ts
@@ -1,5 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
+import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Item } from 'src/app/models/item';
+import { ItemCategory } from 'src/app/models/item-category';
+import { ItemService } from 'src/app/services/item.service';
 
 @Component({
   selector: 'app-report',
@@ -7,16 +12,25 @@ import { Chart } from 'chart.js';
   styleUrls: ['./report.page.scss'],
 })
 export class ReportPage implements OnInit {
+  private readonly _itemCategories: BehaviorSubject<
+    ItemCategory[]
+  > = new BehaviorSubject<ItemCategory[]>([]);
+  get itemCategories(): ItemCategory[] {
+    return this._itemCategories.getValue();
+  }
+  set itemCategories(itemCategories: ItemCategory[]) {
+    this._itemCategories.next(itemCategories);
+  }
+  readonly itemCategories$ = this._itemCategories.asObservable();
   // not really understand this one
   @ViewChild('pieCanvas') pieCanvas: ElementRef;
 
   pieChart: Chart;
 
-  constructor() {}
+  constructor(private itemService: ItemService) {}
 
   ngOnInit() {}
   ionViewWillEnter() {
-    // TODO change the data with legit data
     this.pieChart = new Chart(this.pieCanvas.nativeElement, {
       type: 'pie',
       data: {
@@ -45,8 +59,46 @@ export class ReportPage implements OnInit {
   }
 
   ionViewDidEnter() {
-    this.addData('Pen', 200);
-    this.addData('Minuman', 120);
+    this.itemService
+      .getItems()
+      .pipe(
+        map((data: any) => {
+          const items = data.data;
+
+          items.forEach((data) => {
+            const item = new Item();
+            item.id = data._id;
+            item.category = data.category;
+            item.name = data.name;
+            item.color = data.color;
+            item.quantity = data.quantity;
+
+            const foundItemCategory = this.itemCategories.find(
+              (itemCategory) => itemCategory.name == item.category
+            );
+            if (foundItemCategory === undefined) {
+              const itemCategory = new ItemCategory();
+              itemCategory.name = item.category;
+              itemCategory.items = [];
+              itemCategory.items.push(item);
+              this.itemCategories.push(itemCategory);
+            } else {
+              foundItemCategory.items.push(item);
+            }
+          });
+          return this.itemCategories;
+        })
+      )
+      .subscribe((itemCategories) => {
+        itemCategories.forEach((itemCategory) => {
+          var count: number = 0;
+          itemCategory.items.forEach((item) => {
+            if (item.quantity != Number.POSITIVE_INFINITY)
+              count += item.quantity;
+          });
+          this.addData(itemCategory.name, count);
+        });
+      });
   }
 
   addData(label: string, data: number) {
